6cd673d82ae8555a19a27a18d2301c39f475bbbd
valdeadmario <vladimir.vstup@gmail.com>
2020-04-26 08:42:09 +0300

    Updates

---
+++ precache-manifest.5e0ed16105ba3e927c3671b95cbe665f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224cb940a6e21bafe9302ef0be7a7faf",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "b1754d0ab34602fd67a2",
+    "url": "/calendar/static/css/2.1fb699ea.chunk.css"
+  },
+  {
+    "revision": "42c9bfbb91d0347e673b",
+    "url": "/calendar/static/css/main.1a43586a.chunk.css"
+  },
+  {
+    "revision": "b1754d0ab34602fd67a2",
+    "url": "/calendar/static/js/2.d8c40216.chunk.js"
+  },
+  {
+    "revision": "27da5780f65c52b0b61e20dbfa9a6f02",
+    "url": "/calendar/static/js/2.d8c40216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c9bfbb91d0347e673b",
+    "url": "/calendar/static/js/main.af3da38e.chunk.js"
+  },
+  {
+    "revision": "af58d300f8cce9f49e0f",
+    "url": "/calendar/static/js/runtime-main.51f2034f.js"
+  },
+  {
+    "revision": "ee882a56c82f73e56f41f66c885ad5b9",
+    "url": "/calendar/static/media/calendar.ee882a56.svg"
+  }
+]);
